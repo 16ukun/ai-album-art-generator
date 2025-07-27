3dab0b1e5e8ccf9d69ae0d14b80e4eaf42ca257e
Jeremiah.Osidele <Jeremiah.Osidele@fdmgroup.com>
2025-07-28 00:13:31 +0100

    feat: replase replicate with hugging face, add readme doc

--- backend/src/modules/audio/audio.module.ts
+++ backend/src/modules/audio/audio.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AudioController } from './audio.controller';
 import { AudioService } from './audio.service';
+import { ImageModule } from 'src/image/image.module';
 
 @Module({
+  imports: [ImageModule],
   controllers: [AudioController],
   providers: [AudioService],
 })
